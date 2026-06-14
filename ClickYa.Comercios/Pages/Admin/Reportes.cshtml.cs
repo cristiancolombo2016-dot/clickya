02b525064315cd6128991a98b4e44cf9b9e395ac
Cristian Colombo <cristiancolombo2016@gmail.com>
2026-06-14 18:25:00 -0300

    Panel admin: tarjeta y pagina de Reportes

---
+++ ClickYa.Comercios/Pages/Admin/Reportes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClickYa.Comercios.Pages.Admin
+{
+    public class ReportesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
